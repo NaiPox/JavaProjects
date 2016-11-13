57635415d5edda413c712afe35a4e0deabcf4094
Lennard <lennard@lewil.dk>
2016-11-13 22:55:56 +0100

    File Sync File Sync File Sync File Sync File Sync File Sync File Sync

---
+++ Hello World App/src/hello/world/app/HelloWorldApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.app;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!"2 to standard output.
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!"); // Displays the string
+        
+    }
+    
+}
