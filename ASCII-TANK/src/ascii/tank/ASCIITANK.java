cb51873d26f930036892fa512a2f128ae5d6ad5e
Lennard Martensen Thomsen <Lennard Martensen Thomsen>
2016-11-15 23:57:08 +0100

    UPDATE: First Version of ASCII-TANK. V. 0.0.1

--- ASCII-TANK/src/ascii/tank/ASCIITANK.java
+++ ASCII-TANK/src/ascii/tank/ASCIITANK.java
@@ -16,9 +16,41 @@ public class ASCIITANK {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Defines Scanner
         Scanner Kinput = new Scanner(System.in);
-        
-        
+        System.out.println("Welcome to ASCII-TANK\n"
+                + "Please choose your desired text speed (1 for slow , 2 for medium or 3 for fast): ");
+       int tSpeed, x;
+       tSpeed = Kinput.nextInt();
+        switch (tSpeed) {
+            case 1:
+                x = 75;
+                break;
+            case 2:
+                x = 50;
+                break;
+            case 3:
+                x = 25;
+                break;
+            default:
+                Clear();
+                System.out.println("Valid option not chosen, defaulting to medium text speed.");
+                x = 50;
+                break;
+        }
+      String Controls = ("Placeholder");
+              
+      
+    //Method that clears the console whenever Clear(); Is prompted in the code.
+
+   
+}
+
+public static void Clear()
+    {
+    for(int clear = 0; clear < 50; clear++)
+    {
+    System.out.println("\b");
     }
-    
+    }
 }
