b68a53c878ed9a7c4ddf2667a38bff29ea46d528
Lennard <lennard@lewil.dk>
2016-12-19 11:06:55 +0100

    Update SortF

--- F-Sort/src/f/sort/FSort.java
+++ F-Sort/src/f/sort/FSort.java
@@ -17,12 +17,22 @@ public class FSort {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner Kinput = new Scanner(System.in);
-        String[] SortF = {"A1", "A2", "A3"};
+        System.out.println("Press 1 for F-randomizer.");    
+        System.out.println("Press 2 to mark favourites");
+        System.out.println("Press 3 to see favourites");
+        int x = Kinput.nextInt();
+        
+        if (x == 1)
+        {
+        String[] SortF = {"A1", "A2", "A3","A4","A5","A6","A53"};
         int i;
         i = (int)(Math.random()*3);
-        
-        
         System.out.println(SortF[i]);
+        }
+        else 
+        {
+        System.out.println("W.I.P or invalid choice");
+        }
     }
     
 
